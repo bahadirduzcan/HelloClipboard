63d8c9ab9a9e677fa6da442b6c37b289aacd6378
alisariaslann <125967344+alisariaslann@users.noreply.github.com>
2025-11-22 19:14:09 +0300

    assembly changed

--- HelloClipboard/Properties/AssemblyInfo.cs
+++ HelloClipboard/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,18 +7,18 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HelloClipboard")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Lightweight clipboard sharing tool for Windows. Website: alisariaslan.com.tr")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ali SARIASLAN")]
 [assembly: AssemblyProduct("HelloClipboard")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("© 2025 Ali SARIASLAN")]
+[assembly: AssemblyTrademark("© 2025 Ali SARIASLAN")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e6315deb-779f-420b-94b0-61f39eb5ac2e")]
@@ -31,3 +32,4 @@ using System.Runtime.InteropServices;
 //
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
